50e7387a42c46c939caf95d9168e80cd51e62b21
Cynthia Zhang <zchcynthia@gmail.com>
2020-03-13 13:14:01 -0700

    lin was here

--- UnitThree.java
+++ UnitThree.java
@@ -13,34 +13,41 @@ public class UnitThree
         System.out.println("  When temp = 60..." + temperature(60));
         System.out.println("ANSWER: I only\n");
         System.out.println("-------------------------------------\n");
-        /* // SKILL 2B
+        // SKILL 2C
         System.out.println("SKILL 2B");
-        System.out.println("Consider the following code segment: \n double d = 0.25; \n int i = 3; \n double diff = d - i; \n System.out.print((int)diff-0.5);");
+        System.out.println("Consider the following code segment:");
+        System.out.println("String first = new String(\"duck\");\nString second = new String(\"duck\");\nString third = new String (\"goose\");\nif (first == second)\n{\n  System.out.print(\"A\");\n}\nelse if (second == third)\n{\n System.out.print(\"B\");\n}\nelse if (first.equals(second))\n{\n  System.out.print(\"C\");\n}\nelse if (second.equals(third))\n{\n  System.out.print(\"D\");\n}\nelse\n{\n  System.out.print(\"E\");\n}");
         System.out.println("What is printed as a result of executing the code segment?\n");
         System.out.println("SOLUTION");
-        double d = 0.25;
-        int i =3;
-        double diff = d -i;
-        System.out.println((int)diff - 0.5);
+        System.out.println("  When the code is run..." + questionTwo());
+        System.out.println("ANSWER: C");
         System.out.println("-------------------------------------\n");
-        // SKILL 5B
-        System.out.println("SKILL 5B");
-        System.out.println("Consider the following code segment, which is intended to display 6.0");
-        System.out.println(" double fact1 = 1/2; \n double fact2 = 3*4; \n double product = fact1 * fact2; \n System.out.println(product);");
-        System.out.println("Which of the following best describes the error, if any, in the code segment?\n");
+        
+        // SKILL 4C
+        System.out.println("SKILL 4C");
+        System.out.println("Consider the following code segment. Assume that variables x and y have been declared as int variables and have been assigned integer values.");
+        System.out.println("I.\n\nint result = 0;\nif (x>y)\n{\n  result = x - y;\n  System.out.print(result);\n}\nelse if (x<y)\n{\n  result = y-x;\n  System.out.print(result);\n}\nelse\n{\n  System.out.print(result);\n}");
+        System.out.println("II.\n\n if (x<y)\n{\n  System.out.print(y-x);\n}\nelse\n{\n  System.out.print(x-y);\n}");
+        System.out.println("Which of the following correctly compares the outputs of the two code segments.");
         System.out.println("SOLUTION");
-        System.out.println("When the code runs...");
-        double fact1 = 1/2;
-        double fact2 = 3*4;
-        double product = fact1 * fact2;
-        System.out.println(" " + product); //space added for readability
-        System.out.println("If the numerator of 1/2 is cast as a double...");
-        fact1 = (double)1/2; //defining variables again
-        fact2 = 3*4;
-        product = fact1 * fact2;
-        System.out.println(" " + product);
-        System.out.println("Therefore the answer is: \"Either the numerator or the denominator of the fraction 1 / 2 should be cast as double\"\n\n");
-    */}
+        System.out.println("When I runs for x=1, y=2... " + questionThreeI(1,2));
+        System.out.println("When II runs for x=1, y=2... " + questionThreeII(1,2));
+        System.out.println("When I runs for x=2, y=1... " + questionThreeI(2,1));
+        System.out.println("When II runs for x=2, y=1... " + questionThreeII(2,1));
+        System.out.println("When I runs for x=1, y=1... " + questionThreeI(1,1));
+        System.out.println("When II runs for x=1, y=1... " + questionThreeII(1,1));
+        System.out.println("Therefore, the answer is: \"Code segment I and code segment II produce the same output for all values of x and y\"\n\n");
+        //SKILL 4C 2
+        System.out.println("SKILL 4C 2");
+        System.out.println("In the following expression, j, k, and m are properly declared and initialized int variables.");
+        System.out.println("!((j == k) && (k > m))");
+        System.out.println("Which of the following is equivalent to the expression above?");
+        System.out.println("SOLUTION");
+        System.out.println(" (j != k) || (k <= m)");
+        System.out.println(" One of De Morgan's laws says that !(A && B) is equivalent to !A || !B .");
+        System.out.println(" For example, \"I don't like both apples and oranges\" is the same as \"I don't like apples, or I don't like oranges.\"");
+        System.out.println("-------------------------------------\n");
+    }
     public static String temperature(int temp)
     {
         String weather;
@@ -61,5 +68,61 @@ public class UnitThree
             weather = "warm";
         }
         return weather;
+    }
+    public static String questionTwo()
+    {
+        String first = new String("duck");
+        String second = new String("duck");
+        String third = new String("goose");
+        if (first == second)
+        {
+            return "A";
+        }
+        else if (second == third)
+        {
+            return "B";
+        }
+        else if (first.equals(second))
+        {
+            return "C";
+        }
+        else if (second.equals(third))
+        {
+            return "D";
+        }
+        else
+        {
+            return "E";
+        }
+    }
+    public static int questionThreeI(int x, int y)
+    {
+        int result = 0;
+        if (x > y)
+        {
+            result = x - y;
+            return result;
+        }
+        else if (x<y)
+        {
+            result = y - x;
+            return result;
+        }
+        else
+        {
+            return result;
+        }
+    }
+    public static int questionThreeII(int x, int y)
+    {
+        int result = 0;
+        if (x<y)
+        {
+            return y-x;
+        }
+        else
+        {
+            return x-y;
+        }
     }
 }
